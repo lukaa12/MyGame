c93d2c402a864e83613225865bf719fb2753fd58
lukaa12 <l.wolanin@stud.elka.pw.edu.pl>
2019-05-03 18:27:49 +0200

    MyGame Project for PROZ ver. 0.1.7

--- src/engine/PlayerSteering.java
+++ src/engine/PlayerSteering.java
@@ -8,6 +8,14 @@ public class PlayerSteering implements Updater {
     @Override
     public void update(double deltaTime) {
         int direction;
-//        if()
+        if(model.isRight()&&!model.isLeft()) {
+            if(model.isUp()&&!model.isDown())
+                direction = 45;
+            else if(model.isDown()&&!model.isUp())
+                direction = 135;
+            else
+                direction = 90;
+        }
+
     }
 }
