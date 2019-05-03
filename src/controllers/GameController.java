91b76578741e1b1d90c3f6958e73035493e14567
lukaa12 <l.wolanin@stud.elka.pw.edu.pl>
2019-05-03 14:04:24 +0200

    Starting MyGame Project for PROZ ver. 0.1.5

--- src/controllers/GameController.java
+++ src/controllers/GameController.java
@@ -3,48 +3,62 @@ package controllers;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import models.Player;
 
 import java.io.IOException;
 
+
+
 public class GameController {
     private ViewController viewController;
-    @FXML
     private ImageView playerTransform;
     @FXML
     private Pane newGamePane;
     @FXML
     public void initialize() {
-//        playerTransform.setX(450);
-//        playerTransform.setRotationAxis(new Point3D((playerTransform.getX()+playerTransform.getFitWidth()),
-//                (playerTransform.getY()+playerTransform.getFitHeight()),0.0));
-        playerTransform.setRotate(Math.toRadians(90));
-
+        Player player = new Player();
+        playerTransform = new ImageView(new Image(this.getClass().getResource("/resources/player.png").toString()));
+        newGamePane.getChildren().add(playerTransform);
     }
 
     void setViewController(ViewController viewController) {
         this.viewController = viewController;
-        viewController.getScene().setOnKeyPressed(new EventHandler<>() {
+        this.viewController.getScene().setOnKeyPressed(new EventHandler<>() {
             @Override
             public void handle(KeyEvent keyEvent) {
-                if (keyEvent.getCode() == KeyCode.ESCAPE) {
-//                  System.out.println("ESCAPE"+keyEvent.getCode());
-                    viewController.mainStackPane.getChildren().clear();
-                    FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/MenuScreen.fxml"));
-                    Pane menuPane = null;
-                    try {
-                        menuPane = loader.load();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    MenuController menuController = loader.getController();
-                    menuController.setViewController(viewController);
-                    viewController.mainStackPane.getChildren().add(menuPane);
+                switch (keyEvent.getCode()) {
+                    case ESCAPE:
+                        viewController.mainStackPane.getChildren().clear();
+                        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/MenuScreen.fxml"));
+                        Pane menuPane = null;
+                        try {
+                            menuPane = loader.load();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        MenuController menuController = loader.getController();
+                        menuController.setViewController(viewController);
+                        viewController.mainStackPane.getChildren().add(menuPane);
+                        break;
+                    case W:
 
+                        break;
+                    case A:
+                        break;
+                    case S:
+                        break;
+                    case D:
+                        break;
+                    case SPACE:
+                        break;
+                    case SHIFT:
+                        break;
                 }
+
             }
         });
     }
