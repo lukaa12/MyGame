cf5945125feade14483c2a570608499d56329f1d
lukaa12 <l.wolanin@stud.elka.pw.edu.pl>
2019-05-17 00:05:32 +0200

    MyGame Project for PROZ ver. 0.3.1 log4j first attempt

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,7 +1,6 @@
 package view;
 
 import controllers.ViewController;
-import engine.GameEngine;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,10 +8,14 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 
 public class Main extends Application {
+    static Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
+        DOMConfigurator.configure("log4j2.xml");
         launch(args);
     }
     @Override
@@ -27,6 +30,7 @@ public class Main extends Application {
         stage.setScene(scene);
         ViewController viewController = loader.getController();
         viewController.setScene(scene);
+        logger.info("Log4j appender configuration is successful !!");
         stage.show();
     }
 }
