7e2a557cb1b82fb044ba285769baccb55d17abef
lukaa12 <l.wolanin@stud.elka.pw.edu.pl>
2019-05-18 14:19:33 +0200

    MyGame Project for PROZ ver. 0.3.3

--- src/controllers/OptionsController.java
+++ src/controllers/OptionsController.java
@@ -9,7 +9,13 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import view.Main;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.IOException;
 
 public class OptionsController {
@@ -26,6 +32,25 @@ public class OptionsController {
 
     @FXML
     public void initialize() {
+        DocumentBuilder documentBuilder = null;
+        Document doc = null;
+        Node soundMenu = null;
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            documentBuilder = dbFactory.newDocumentBuilder();
+            doc = documentBuilder.parse(this.getClass().getResourceAsStream("/resources/settings.xml"));
+            doc.getDocumentElement().normalize();
+            Main.logger.info(doc.getDocumentElement().getNodeName()+" read by options controller.");
+            soundMenu = doc.getElementsByTagName("menuMusic").item(0);
+            Main.logger.info(soundMenu.getNodeName()+" : +soundMenu.getAttributes().item(0).getNodeName()");
+            if(soundMenu.getAttributes().getNamedItem("enabled").equals("true")) {
+                menuMusic.setSelected(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Node finalSoundMenu = soundMenu;
+        finalSoundMenu.getAttributes().getNamedItem("enabled").setTextContent("false");
         EventHandler<ActionEvent> optionsHandler = actionEvent -> {
             if(actionEvent.getSource().equals(back)) {
 
@@ -40,6 +65,14 @@ public class OptionsController {
                 MenuController menuController = loader.getController();
                 menuController.setViewController(viewController);
                 viewController.mainStackPane.getChildren().add(menuPane);
+            }
+            if(actionEvent.getSource().equals(menuMusic)) {
+                if(menuMusic.isSelected()) {
+                    finalSoundMenu.getAttributes().getNamedItem("enabled").setTextContent("true");
+                }
+                else {
+                    finalSoundMenu.getAttributes().getNamedItem("enabled").setTextContent("false");
+                }
             }
         };
         back.addEventHandler(ActionEvent.ACTION,optionsHandler);
